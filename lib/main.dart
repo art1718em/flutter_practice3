f5c54faf3764b32b9edb8aa4af1d91223a90314b
kiryanovartem <art1718em@yandex.ru>
2025-10-03 20:09:40 +0300

    Виджет Row

--- lib/main.dart
+++ lib/main.dart
@@ -30,8 +30,9 @@ class MyHomePage extends StatelessWidget {
         title: const Text('Text & ElevatedButton Demo'),
       ),
       body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Text(
               'Тестируем виджет Text',
@@ -40,20 +41,10 @@ class MyHomePage extends StatelessWidget {
                 fontWeight: FontWeight.bold,
                 color: Colors.deepPurple,
                 letterSpacing: 2.0,
-                wordSpacing: 4.0,
-                shadows: [
-                  Shadow(
-                    offset: Offset(2.0, 2.0),
-                    blurRadius: 4.0,
-                    color: Colors.grey,
-                  ),
-                ],
                 decoration: TextDecoration.underline,
-                decorationColor: Colors.deepPurple,
                 decorationThickness: 2.0,
               ),
               textAlign: TextAlign.center,
-              maxLines: 2,
               overflow: TextOverflow.ellipsis,
               semanticsLabel: 'Тестовый текст для демонстрации виджета Text',
             ),
@@ -62,11 +53,6 @@ class MyHomePage extends StatelessWidget {
               style: ButtonStyle(
                 backgroundColor: WidgetStatePropertyAll(Colors.deepPurple),
                 foregroundColor: WidgetStatePropertyAll(Colors.white),
-                elevation: WidgetStatePropertyAll(8.0),
-                shadowColor: WidgetStatePropertyAll(Colors.deepPurpleAccent),
-                padding: WidgetStatePropertyAll(EdgeInsets.symmetric(horizontal: 32.0, vertical: 16.0)),
-                minimumSize: WidgetStatePropertyAll(Size(200, 60)),
-                maximumSize: WidgetStatePropertyAll(Size(300, 80)),
                 shape: WidgetStatePropertyAll(
                   RoundedRectangleBorder(
                     borderRadius: BorderRadius.all(Radius.circular(25.0)),
